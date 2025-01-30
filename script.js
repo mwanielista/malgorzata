06ef69ab17f2a319932a2591f9e371fa054b7b08
Michal Wanielista <michal.wanielista20@gmail.com>
2025-01-30 20:50:01 +0100

    update

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.content');
 
-// tabs.forEach(tab => {
-//   tab.addEventListener('click', () => {
-//     tabs.forEach(t => t.classList.remove('active'));
-//     contents.forEach(c => c.classList.remove('active'));
-
-//     tab.classList.add('active');
-//     document.getElementById(tab.dataset.tab).classList.add('active');
-//   });
-// });
 
 const textArea = document.getElementById("text1");
-
 textArea.addEventListener("mouseenter", () => {
   textArea.classList.add("active");
 });
-
 textArea.addEventListener("mouseleave", () => {
   textArea.classList.remove("active");
 });
 
 const textArea2 = document.getElementById("text2");
-
 textArea2.addEventListener("mouseenter", () => {
   textArea2.classList.add("active");
 });
-
 textArea2.addEventListener("mouseleave", () => {
   textArea2.classList.remove("active");
 });
